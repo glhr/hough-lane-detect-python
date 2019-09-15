adf6ab058a96baf6cd45901262493c27cf4e510f
Unknown <grenblot@gmail.com>
2019-09-15 16:39:22 +0200

    implemented Canny edge detector

--- main.py
+++ main.py
@@ -14,21 +14,23 @@ from hough import do_hough
 # open test.jpg as grayscale array
 img = np.array(Image.open('test.jpg').convert("L"))
 
-# blurred = do_gaussian(img) # implement function in gaussian.py
-blurred = cv2.GaussianBlur(img, (5,5), 0) # OpenCV built-in function, for testing only
+img = img[250:,:]  # only keep bottom part of image
 
-# edges = do_canny(blurred) # implement function in canny.py
-edges = cv2.Canny(blurred, 50, 150) # OpenCV built-in function, for testing only
+blurred = do_gaussian(img) # implement function in gaussian.py
+blurred_cv = cv2.GaussianBlur(img, (5,5),1) # OpenCV built-in function, for testing only
+
+edges = do_canny(blurred_cv) # implement function in canny.py
+edges_cv = cv2.Canny(blurred_cv, 50, 150) # OpenCV built-in function, for testing only
 
 detected_lines = do_hough(edges) # implement function in hough.py
 
 # plot results
 plt.subplot(221),plt.imshow(img, cmap='gray')
 plt.title('Original image'), plt.xticks([]), plt.yticks([])
-plt.subplot(222),plt.imshow(blurred, cmap='gray')
-plt.title('Blurred image (Gaussian filter)'), plt.xticks([]), plt.yticks([])
-plt.subplot(223),plt.imshow(edges, cmap='gray')
-plt.title('Canny edge detection'), plt.xticks([]), plt.yticks([])
-plt.subplot(224),plt.imshow(detected_lines, cmap='gray')
-plt.title('Line detection (Hough transform)'), plt.xticks([]), plt.yticks([])
+plt.subplot(222),plt.imshow(blurred_cv, cmap='gray')
+plt.title('Blurred image (Gaussian filter OpenCV)'), plt.xticks([]), plt.yticks([])
+plt.subplot(223),plt.imshow(edges_cv, cmap='gray')
+plt.title('Canny edge detection (OpenCV)'), plt.xticks([]), plt.yticks([])
+plt.subplot(224),plt.imshow(edges, cmap='gray')
+plt.title('Canny edge detection (homemade)'), plt.xticks([]), plt.yticks([])
 plt.show()
