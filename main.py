30ba1911a8ef168b7691842ad22a417920c50ec0
glhr <grenblot@gmail.com>
2019-11-07 16:23:59 +0100

    downsample further for faster processing

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import glob
 import numexpr as ne
 
 GAUSSIAN_SIZE = 5 # kernel size
-CANNY_LOW = 5
-CANNY_HIGH = 15
+CANNY_LOW = 8
+CANNY_HIGH = 20
 
 colors = {
             'white':(255, 255, 255),
@@ -25,8 +25,8 @@ def preprocessing(image):
 
     # downsample image
     h,w = img.shape[:2]
-    desired_w = 250
-    small_to_large_image_size_ratio = 0.3125
+    #desired_w = 150
+    small_to_large_image_size_ratio = 0.1
     img = cv2.resize(img,
                        (0,0), # set fx and fy, not the final size
                        fx=small_to_large_image_size_ratio,
@@ -45,12 +45,11 @@ def plot_line(a, b, rho, img, opacity=0.8, color='red'):
     #print(a, b)
     return img
 
-
 def is_theta_in_range(theta):
     return (theta < np.deg2rad(-10) and theta > np.deg2rad(-70)) or (theta > np.deg2rad(10) and theta < np.deg2rad(70))
 
 def theta_ranges_from_lane_angle(lane_angle):
-    theta_offset = 45
+    theta_offset = 70
     print('Theta limits:',lane_angle-theta_offset,lane_angle+theta_offset)
     return np.deg2rad(np.concatenate((np.arange(lane_angle-theta_offset,-10),np.arange(10,lane_angle+theta_offset))))
 
@@ -94,16 +93,10 @@ def do_hough_straightline(orig, img, lane_angle, n_lines, max_area, plot=False):
     while n <= n_lines and iterations < max_iterations:
         # print(iterations)
         # find maximum point in accumulator
-        # result = np.where(accumulator == np.max(accumulator))
-        # print("max. in accumulator:", np.max(accumulator))
-        # maxCoordinates = list(zip(result[0], result[1]))
-        # print(maxCoordinates)
 
         max_index = np.argmax(accumulator)  # 2d index of maximum point in accumulator
-        theta_index = np.uint64(max_index % accumulator.shape[1])
-        rho_index = np.uint64(max_index / accumulator.shape[1])
-
-        # cv2.circle(accumulator, (rho_index,theta_index), 50, (0,255,0), thickness=5, lineType=8, shift=0)
+        theta_index = np.uint16(max_index % accumulator.shape[1])
+        rho_index = np.uint16(max_index / accumulator.shape[1])
 
         ang = thetas[theta_index]
         rho = rhos[rho_index]
@@ -116,32 +109,30 @@ def do_hough_straightline(orig, img, lane_angle, n_lines, max_area, plot=False):
         lane_side[n-1] = (lane_start[n-1] < middle)
 
         print(f"- Lane {n}: Cartesion form (ax+b): {a:.2f} * x + {b:.2f}")
-        # print(f"\t starting at y = ", lane1_start)
         print(f"- Lane {n}: Theta {np.rad2deg(ang):.2f} - Rho {rho:.2f}")
-
-        # print(f"Hough coordinates: rho {rho:.2f}  theta(rad) {ang:.2f}  theta(deg) {np.rad2deg(ang)}")
 
         if n == 1:
             color_edges = plot_line(a, b, rho, color_edges, color='green')
+            #accumulator = remove_area_around_max(accumulator,max_area,(rho_index,theta_index))
             n += 1
         elif n == 2:
-            if (lane_side[n-1] != lane_side[n-2]) and ((lane_end[n-1] > lane_end[n-2] and lane_start[n-1] > lane_start[n-2]) or (
-                    lane_end[n-1] < lane_end[n-2] and lane_start[n-1] < lane_start[n-2])):
+            if (lane_side[n-1] != lane_side[n-2]) and \
+                    ((lane_end[n-1] > lane_end[n-2] and lane_start[n-1] > lane_start[n-2]) \
+                or  (lane_end[n-1] < lane_end[n-2] and lane_start[n-1] < lane_start[n-2])):
                 lane_color = 'blue'
                 n += 1
             else:
                 lane_color = 'red'
+                #accumulator = remove_area_around_max(accumulator,max_area,(rho_index,theta_index))
             color_edges = plot_line(a, b, rho, color_edges, color=lane_color)
 
-        prev_ang = ang
-        prev_rho = rho
-
-        remove_area = max_area
-        for i in range(np.int(rho_index - remove_area), np.int(rho_index + remove_area + 1)):
+        for i in range(np.int(rho_index - max_area), np.int(rho_index + max_area + 1)):
             try:
-                accumulator[i][np.int(theta_index - remove_area):np.int(theta_index + remove_area)] = 0
+                accumulator[i][np.int(theta_index - max_area):np.int(theta_index + max_area)] = 0
             except:
                 pass
+        prev_ang = ang
+        prev_rho = rho
 
         iterations += 1
 
@@ -154,12 +145,16 @@ def draw_lanes(image_path):
     image = cv2.imread(image_path)
     image = preprocessing(image)
     edges = cv2.Canny(image, CANNY_LOW, CANNY_HIGH, None, 3)
-    return do_hough_straightline(image, edges, lane_angle=0, n_lines=2, max_area=10, plot=False)
+    return image, do_hough_straightline(image, edges, lane_angle=0, n_lines=2, max_area=5, plot=False)
 
 
-for path in glob.iglob('cam_data/ir/*.png'):
-    cv2.imshow('lanes',draw_lanes(path))
-    cv2.waitKey(0)
+for path in glob.iglob('/home/zacefron/Desktop/golfcart-sensorfusion/sensorfusion/cam_data/ir*.png'):
+    orig,lanes = draw_lanes(path)
+    cv2.imshow('orig',orig)
+    cv2.imshow('lanes',lanes)
+    k = cv2.waitKey(0) & 0xFF
+    if k == ord('q'):
+        break
 
 # detect_lane('canny_output/cannylane_test.jpg')
 #detect_lane('cam_data/ir/ircam1571746678401506290.png')
